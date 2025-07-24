d20b7f8498deaac51877c8de7b3817d19f0bd7b8
nicolasnicholson <nicolasnicholson191@gmail.com>
2025-07-24 01:07:22 -0300

    disney-clone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
-import { auth, provider } from "../firebase.js";
+import { auth, provider } from "../firebase";
 import {
   selectUserName,
   selectUserPhoto,
